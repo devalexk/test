599a57843317852855b8a21fc7ddf490d5f3cf3d
Oleh Shklyar <pio_ua@ukr.net>
2016-11-21 23:00:09 +0200

    hello from interface

---
+++ src/main/java/edu/olezha/jsandbox/java8/intf/Main.java
@@ -0,0 +1,12 @@
+package edu.olezha.jsandbox.java8.intf;
+
+/**
+ * Created by oleh on 21.11.16.
+ */
+public interface Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello there!");
+    }
+
+}
