a4348f69118ab9f183654821b56e8a0e2dc460b3
Oleh Shklyar <pio_ua@ukr.net>
2018-01-29 20:35:37 +0200

    util.date

---
+++ src/main/java/edu/olezha/jsandbox/core/date/util/Main.java
@@ -0,0 +1,28 @@
+package edu.olezha.jsandbox.core.date.util;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Date;
+import java.util.TimeZone;
+
+@Slf4j
+public class Main {
+
+    public static void main(String[] args) {
+        Date date = new Date();
+        log.info("My Now: {}", date);
+
+        Date date0 = new Date(0);
+        log.info("My Zero time: {}", date0);
+
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        log.info("UTC Now: {}", date);
+        log.info("UTC Zero time: {}" + System.lineSeparator(), date0);
+
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Kiev"));
+        Date myBirthday = new Date(83, 8, 20);
+        log.info("I was born in {}", myBirthday);
+        TimeZone.setDefault(TimeZone.getTimeZone("America/New_York"));
+        log.info("I was born in {}", myBirthday);
+    }
+}
