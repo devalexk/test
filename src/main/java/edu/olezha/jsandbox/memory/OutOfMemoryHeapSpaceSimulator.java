4209c90dfce944b3b1d81ef7192ce824b5367c0b
Oleh Shklyar <pio_ua@ukr.net>
2018-04-09 12:47:15 +0300

    heap out of memory

---
+++ src/main/java/edu/olezha/jsandbox/memory/OutOfMemoryHeapSpaceSimulator.java
@@ -0,0 +1,23 @@
+package edu.olezha.jsandbox.memory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OutOfMemoryHeapSpaceSimulator {
+
+    private static Map<String, OutOfMemoryHeapSpaceSimulator> leackContainer = new HashMap<>();
+    private static final String dataPrefix = "DATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATA";
+
+    public static void main(String[] args) {
+        try {
+            for (int i = 0; i < 10000000; i++) {
+                String data = dataPrefix + i;
+                leackContainer.put(data, new OutOfMemoryHeapSpaceSimulator());
+            }
+        }
+        catch (Throwable e) {
+            System.out.println("OutOfMemoryHeapSpaceSimulator Failure: " + e);
+        }
+        System.out.println("OutOfMemoryHeapSpaceSimulator END");
+    }
+}
