66b57ad355e212a6de14bc1260797af4f3c42ace
Oleh Shklyar <pio_ua@ukr.net>
2018-04-15 22:22:32 +0300

    OutOfMemoryError slow execution

--- src/main/java/edu/olezha/jsandbox/memory/OutOfMemoryHeapSpaceSimulator.java
+++ src/main/java/edu/olezha/jsandbox/memory/OutOfMemoryHeapSpaceSimulator.java
@@ -10,12 +10,13 @@ public class OutOfMemoryHeapSpaceSimulator {
 
     public static void main(String[] args) {
         try {
+            Object lock = new Object();
             for (int i = 0; i < 10000000; i++) {
                 String data = dataPrefix + i;
                 leackContainer.put(data, new OutOfMemoryHeapSpaceSimulator());
+                Thread.sleep(1);
             }
-        }
-        catch (Throwable e) {
+        } catch (Throwable e) {
             System.out.println("OutOfMemoryHeapSpaceSimulator Failure: " + e);
         }
         System.out.println("OutOfMemoryHeapSpaceSimulator END");
