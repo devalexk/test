e1ac74c2e00a64dbad2c9321bbd761e0f134a51e
Oleh <pio_ua@ukr.net>
2018-06-14 17:35:22 +0300

    tricks

---
+++ src/main/java/edu/olezha/jsandbox/tricks/Integers.java
@@ -0,0 +1,8 @@
+package edu.olezha.jsandbox.tricks;
+
+public class Integers {
+
+    public static void main(String[] args) {
+        System.out.println(((Integer) 1 == (Integer) 1) + " " + ((Integer) 222 == (Integer) 222));
+    }
+}
