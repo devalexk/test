ba7dcbdd3aab0afec2b6c4420062bf8c650e94ba
Oleh Shklyar <pio_ua@ukr.net>
2018-06-16 19:47:59 +0300

    bitwise multiplication

--- src/main/java/edu/olezha/jsandbox/core/bit/Multiply.java
+++ src/main/java/edu/olezha/jsandbox/core/bit/Multiply.java
@@ -1,4 +1,27 @@
 package edu.olezha.jsandbox.core.bit;
 
+import java.math.BigInteger;
+
 public class Multiply {
+
+    public static void main(String[] args) {
+        int x = 7915541, y = 53175250;
+        System.out.println(x + " * " + y + "  = " + multiply(x, y));
+        System.out.println("BigInteger.multiply = "
+                + BigInteger.valueOf(x).multiply(BigInteger.valueOf(y)));
+    }
+
+    public static long multiply(int a, int b) {
+        long result = 0, x = a, y = b;
+
+        while (y != 0) {
+            if ((y & 1) == 1) {
+                result += x;
+            }
+            x <<= 1;
+            y >>>= 1;
+        }
+
+        return result;
+    }
 }
--- src/main/java/edu/olezha/jsandbox/core/bit/MultiplyLong.java
+++ src/main/java/edu/olezha/jsandbox/core/bit/MultiplyLong.java
@@ -1,4 +1,44 @@
 package edu.olezha.jsandbox.core.bit;
 
+import java.math.BigInteger;
+
 public class MultiplyLong {
+
+    public static void main(String[] args) {
+        long x = 99689463345168L, y = 254236690803646L;
+        System.out.println(x + " * " + y + "  = " + multiply(x, y));
+        System.out.println("BigInteger.multiply               = "
+                + BigInteger.valueOf(x).multiply(BigInteger.valueOf(y)));
+    }
+
+    /**
+     * for non-negative operands
+     */
+    public static BigInteger multiply(long x, long y) {
+        System.out.println(Long.toBinaryString(x) + "  *  " + Long.toBinaryString(y));
+
+        final long x_hi = x >>> 32;
+        final long y_hi = y >>> 32;
+        final long x_lo = x & 0xFFFFFFFFL;
+        final long y_lo = y & 0xFFFFFFFFL;
+        System.out.println(Long.toBinaryString(x_hi) + " " + Long.toBinaryString(x_lo)
+                + " *  " + Long.toBinaryString(y_hi) + "   " + Long.toBinaryString(y_lo));
+
+        long highHalf = x_lo * y_lo;
+        highHalf >>>= 32;
+        highHalf += x_hi * y_lo + x_lo * y_hi;
+        highHalf >>>= 32;
+        highHalf += x_hi * y_hi;
+
+        long lowHalf = 0;
+        while (y != 0) {
+            if ((y & 1) == 1) {
+                lowHalf += x;
+            }
+            x <<= 1;
+            y >>>= 1;
+        }
+
+        return new BigInteger(Long.toBinaryString(highHalf) + Long.toBinaryString(lowHalf), 2);
+    }
 }
