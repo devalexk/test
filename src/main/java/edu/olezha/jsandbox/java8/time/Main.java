a622d2718799ca68860d36e873ee0fb60163d46c
Oleh <pio_ua@ukr.net>
2018-06-20 14:50:10 +0300

    Day Of The Programmer Date

--- src/main/java/edu/olezha/jsandbox/java8/time/Main.java
+++ src/main/java/edu/olezha/jsandbox/java8/time/Main.java
@@ -2,6 +2,7 @@ package edu.olezha.jsandbox.java8.time;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
 
@@ -18,5 +19,27 @@ public class Main {
 
         LocalDate localDate2 = localDate.plusMonths(1);
         System.out.println(localDate2 + " " + localDate);
+
+        int increment = 27;
+        for (int year = 1700; year < 2700; year += increment) {
+            if (year > 1918) {
+                LocalDate localDate3 = LocalDate.ofYearDay(year, 256);
+                System.out.println(localDate3.format(DateTimeFormatter.ofPattern("dd.LL.yyyy")));
+                increment = 101;
+            } else {
+                System.out.println(getDayOfTheProgrammerDate(year));
+            }
+        }
+    }
+
+    private static String getDayOfTheProgrammerDate(int year) {
+        String jc = "Julian calendar: ";
+        if (year == 1918)
+            return jc + "26.09.1918";
+        else if (((year <= 1917) && (year % 4 == 0)) ||
+                ((year > 1918) && (year % 400 == 0 || ((year % 4 == 0) & (year % 100 != 0)))))
+            return jc + "12.09." + year;
+        else
+            return jc + "13.09." + year;
     }
 }
