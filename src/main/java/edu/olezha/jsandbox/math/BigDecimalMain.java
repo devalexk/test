46de7c0f6ea40abf53a71c33d01cb502ffe5685c
Oleh <pio_ua@ukr.net>
2018-10-05 20:08:44 +0300

    cleanNumbersString

--- src/main/java/edu/olezha/jsandbox/math/BigDecimalMain.java
+++ src/main/java/edu/olezha/jsandbox/math/BigDecimalMain.java
@@ -11,6 +11,17 @@ public class BigDecimalMain {
         System.out.println(price);
         
         System.out.println(price.movePointRight(2).intValue());
+
+        System.out.println(new BigDecimal(cleanNumbersString("abc1,23..456.789")));
     }
 
+    private static String cleanNumbersString(String s) {
+        int lastDotPosition = s.lastIndexOf('.'), lastCommaPosition = s.lastIndexOf(',');
+        int decimalSeparatorPosition = lastDotPosition > lastCommaPosition ? lastDotPosition : lastCommaPosition;
+        StringBuilder cleanNumbersString = new StringBuilder(s.length());
+        for (int i = 0; i < s.length(); i++)
+            if (i == decimalSeparatorPosition || Character.isDigit(s.charAt(i)))
+                cleanNumbersString.append(s.charAt(i));
+        return cleanNumbersString.toString();
+    }
 }
