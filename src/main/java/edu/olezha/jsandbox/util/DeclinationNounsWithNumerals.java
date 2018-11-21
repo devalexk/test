33356a75f273df832a41adfce21114cb3bad6089
Oleh <pio_ua@ukr.net>
2018-11-21 12:13:29 +0200

    clean

--- src/main/java/edu/olezha/jsandbox/util/DeclinationNounsWithNumerals.java
+++ src/main/java/edu/olezha/jsandbox/util/DeclinationNounsWithNumerals.java
@@ -2,8 +2,6 @@ package edu.olezha.jsandbox.util;
 
 import com.ibm.icu.text.MessageFormat;
 
-import java.util.Locale;
-
 public class DeclinationNounsWithNumerals {
 
     public static void main(String[] args) {
@@ -12,9 +10,9 @@ public class DeclinationNounsWithNumerals {
                 "many{# публікацій}" +
                 "other{# публікація}}";
 
-        MessageFormat msgFormat = new MessageFormat(pattern, new Locale("uk"));
+        MessageFormat msgFormat = new MessageFormat(pattern);
 
-        for (int i : new int[] {0, 1, 2, 3, 4, 5, 6, 7, 10, 11, 15, 17, 22, 39, 51, 101, 111, 121, 200, 201, 1010, 1011, 1012})
+        for (int i : new int[] {0, 1, 2, 3, 4, 5, 6, 7, 10, 11, 15, 17, 21, 22, 39, 51, 101, 111, 121, 200, 201, 1010, 1011, 1012})
             System.out.println(msgFormat.format(new Object[] {i}));
     }
 }
