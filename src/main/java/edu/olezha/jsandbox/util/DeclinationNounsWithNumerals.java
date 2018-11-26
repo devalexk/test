8a6de787e3b4f6a55a7af4dc5b88b76ac40b5774
Oleh <pio_ua@ukr.net>
2018-11-26 12:00:11 +0200

    DeclinationNounsWithNumerals depends on locale

--- src/main/java/edu/olezha/jsandbox/util/DeclinationNounsWithNumerals.java
+++ src/main/java/edu/olezha/jsandbox/util/DeclinationNounsWithNumerals.java
@@ -2,6 +2,8 @@ package edu.olezha.jsandbox.util;
 
 import com.ibm.icu.text.MessageFormat;
 
+import java.util.Locale;
+
 public class DeclinationNounsWithNumerals {
 
     public static void main(String[] args) {
@@ -11,8 +13,14 @@ public class DeclinationNounsWithNumerals {
                 "other{# публікація}}";
 
         MessageFormat msgFormat = new MessageFormat(pattern);
+        MessageFormat msgFormatEn = new MessageFormat(pattern, new Locale("en"));
+        MessageFormat msgFormatUk = new MessageFormat(pattern, new Locale("uk"));
 
-        for (int i : new int[] {0, 1, 2, 3, 4, 5, 6, 7, 10, 11, 15, 17, 21, 22, 39, 51, 101, 111, 121, 200, 201, 1010, 1011, 1012})
-            System.out.println(msgFormat.format(new Object[] {i}));
+        for (int i : new int[] {-1, 0, 1, 2, 3, 4, 5, 6, 7, 10, 11, 15, 17, 21, 22, 39, 51, 101, 111, 121, 200, 201, 1010, 1011, 1012}) {
+            Object[] obj = {i};
+            System.out.println(msgFormat.format(obj) + " && " +
+                    msgFormatUk.format(obj) + " && " +
+                    msgFormatEn.format(obj));
+        }
     }
 }
